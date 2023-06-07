afca01134aa11dbc8420f08e33ec01c934610f68
Rodrigo-Brasileiro <rm98952@fiap.com.br>
2023-06-07 01:08:13 -0300

    commit dark mode

--- js/pastaHtml.js
+++ js/pastaHtml.js
@@ -5,3 +5,26 @@ function animar() {
     btnMenu.classList.toggle("ativar")
     menuDiv.classList.toggle("abrir")
 }
+// =============================== DARK MODE ===============================
+const btn = document.querySelector('#btn-tema');
+const body = document.body;
+const imagemTema = document.getElementById('imagem-tema');
+const logoApagado = document.querySelector('#logo-apagado');
+let modoEscuro = false;
+btn.addEventListener("click", () => {
+  if (!modoEscuro) {
+    modoEscuro = true;
+    body.classList.add("dark");
+    dataHora.classList.add('dark');
+    imagemTema.src = '../img/sol-icon-removebg-preview.png';
+    imagemTema.alt = 'sol(light-mode)';
+    logoApagado.src = '../img/Logo(SemFundo)padrao.png';
+  } else {
+    modoEscuro = false;
+    body.classList.remove("dark");
+    dataHora.classList.remove('dark');
+    imagemTema.src = '../img/lua-icon-removebg-preview.png';
+    imagemTema.alt = 'lua(dark-mode)';
+    logoApagado.src = '../img/Logo(SemFundo)preto.png';
+  }
+});
