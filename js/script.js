76812856a2180ada76d818f80b1cf8b2334f5aa9
Rodrigo-Brasileiro <rm98952@fiap.com.br>
2023-06-07 01:07:22 -0300

    commit carrosel

--- js/script.js
+++ js/script.js
@@ -58,3 +58,23 @@ atualizarDataHora();
 
 // Atualiza a data e hora a cada segundo
 setInterval(atualizarDataHora, 1000);
+// =============================== CARROSSEL ===============================
+const radio = document.querySelector(".manual-btn")
+let cont = 1
+
+document.querySelector("#radio1").checked = true
+
+setInterval(()=> {
+    proximaImg()
+}, 4000)
+
+function proximaImg() {
+    cont++
+
+    if(cont > 4) {
+        cont = 1
+    }
+
+    document.querySelector("#radio"+cont).checked = true
+}
+
