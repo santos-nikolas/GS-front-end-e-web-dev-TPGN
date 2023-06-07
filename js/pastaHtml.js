e8cbe7077168c577ed985da0d21a3f1ae5ee27fc
Rodrigo-Brasileiro <rm98952@fiap.com.br>
2023-06-07 01:07:46 -0300

    menu mobile

---
+++ js/pastaHtml.js
@@ -0,0 +1,7 @@
+// =============================== MENU MOBILE ===============================
+function animar() {
+    const btnMenu = document.querySelector("#btn-menu")
+    const menuDiv = document.querySelector("#menu-mobile")
+    btnMenu.classList.toggle("ativar")
+    menuDiv.classList.toggle("abrir")
+}
